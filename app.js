0d632f8fd8940ba2fa59acbfda54e975114c8217
Evette Cheung <FrancFranc@gmx.com>
2017-04-24 14:02:59 -0700

    Goat images included. Array built.  Images URL doesnt work.

--- app.js
+++ app.js
@@ -1,62 +1,33 @@
-'use strict';
+//
 
-var goats = [];
-var guardians = [];
+var goatImages= [
+  new goatImagesFunc('goat_1', 'Goat_1.jpg'),
+  new goatImagesFunc('goat_2', 'Goat_2.jpg'),
+  new goatImagesFunc('goat_3', 'Goat_3.jpg'),
+  new goatImagesFunc('goat_4', 'Goat_4.jpg'),
+  new goatImagesFunc('goat_5', 'Goat_5.jpg'),
+  new goatImagesFunc('goat_6', 'Goat_6.jpg'),
+  new goatImagesFunc('goat_7', 'Goat_7.jpg'),
+  new goatImagesFunc('goat_8', 'Goat_8.jpg'),
+  new goatImagesFunc('goat_9', 'Goat_9.jpg')
+];
 
-// grab goat form from html and assigning it to var goatForm
-// add event listener
-var goatForm = document.getElementById('goat-form');
-goatForm.addEventListener('submit', handleGoatFormSubmit);
 
-// constructor for Goat in need
-function Goat(goatName, goatAge, goatLocation, serviceNeeded, contact, src) {
-  this.goatName = goatName;
-  this.goatAge = goatAge;
-  this.goatLocation = goatLocation;
-  this.serviceNeeded = serviceNeeded;
-  this.contact = contact;
-  this.goatImage = src;
+function goatImagesFunc(name, filename) {
+  this.name = name;
+  this.filename = 'img/goat_images/' + filename;
 }
 
+var imageUL = document.createElement('ul');
+mouseover_images.appendChild(imageUL);
 
-// contructor for Goat Guardian
-function Guardian(author, authorLocation, authorContact, serviceOffered){
-  this.author = author;
-  this.authorLocation = authorLocation;
-  this.authorContact = authorContact;
-  this.serviceOffered = serviceOffered;
+var imageElement;
+for(var i=0; i<goatImages.length; i++) {
+
+  imageElement = document.createElement('img');
+  imageLI = document.createElement('li');
+  imageElement.src = goatImages.src;
+  imageLI.appendChild(imageElement);
+  imageUL.appendChild(imageLI);
+
 }
-
-function handleGoatFormSubmit(event) {
-  event.preventDefault();
-  var goatForm = event.target;
-
-  // grab input data and assign to variables
-  var nameOfGoat = goatForm.nameOfGoat.value;
-  var ageOfGoat = goatForm.ageOfGoat.value;
-  var locationOfGoat = goatForm.locationOfGoat.value;
-  var contactOfGoat = goatForm.contactOfGoat.value;
-  var serviceNeeded = goatForm.serviceNeeded.value;
-
-// storing the value of the input in an empty array of the constructor
-  var addGoat = new Goat(nameOfGoat, ageOfGoat, locationOfGoat, contactOfGoat, serviceNeeded);
-  goats.push(addGoat);
-
-  goatForm.reset();
-}
-
-
-// var goatInNeed = document.getElementById('goat-in-need');
-// var goatGuardian = document.getElementById('goat-guardian');
-
-// goatInNeed.addEventListener('click', functionName);
-// goatGuardian.addEventListener('click', secondFunctionName);
-
-// function selectForm(event) {
-//   if(goatInNeed.checked) {
-//     console.log('show goat in need form', event);
-//   } else if(goatGuardian.checked) {
-//     console.log('show goat guardian form', event);
-//   } else {
-//     console.log('something went wrong');
-//   }
