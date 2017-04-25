3982287a1421cd36b843bc41bf36eaaa6166f34d
Dustin Byers <dbyers84@gmail.com>
2017-04-24 17:08:14 -0700

    fixed images showing on goat page: WORKING PROTOTYPE

--- goatImagesSubmitted.js
+++ goatImagesSubmitted.js
@@ -7,26 +7,6 @@ try {
 } catch(error){
   console.log('problem getting info', error);
 }
-// Create objects for all 9 goats images after user press 'Submit' button
-//
-// var goats = [
-//   new goats('goat_1', 'Goat_1.jpg'),
-//   new goats('goat_2', 'Goat_2.jpg'),
-//   new goats('goat_3', 'Goat_3.jpg'),
-//   new goats('goat_4', 'Goat_4.jpg'),
-//   new goats('goat_5', 'Goat_5.jpg'),
-//   new goats('goat_6', 'Goat_6.jpg'),
-//   new goats('goat_7', 'Goat_7.jpg'),
-//   new goats('goat_8', 'Goat_8.jpg'),
-//   new goats('goat_9', 'Goat_9.jpg')
-// ];
-
-// constructor for the goatImages
-// function goats(name, filename) {
-//
-//   this.name = name;
-//   this.filename = 'img/goat_images/' + filename;
-// }
 
 // access <div>, create <ul> element in html, and append it to <div>
 var mouseoverImages = document.getElementById('mouseover_images');
@@ -37,10 +17,10 @@ mouseoverImages.appendChild(imageUL);
 var imageElement;
 var imageLI;
 
-for(var i=0; i<goats.length; i++) {
+for(var i=0; i <goats.length; i++) {
   imageElement = document.createElement('img');
   imageLI = document.createElement('li');
-  imageElement.src = goats[i].src;
+  imageElement.src = goats[i].goatImage;
   imageLI.appendChild(imageElement);
   imageUL.appendChild(imageLI);
 }
